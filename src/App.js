1d6cfc2ea091010223300e2d7c1be66c2ba75c83
Andriy Sagan <aenain395@gmail.com>
2018-03-31 22:12:54 +0300

    Andriy (#7)
     fix input component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import Button from "./components/Button";
 import Input from "./components/Input";
 
 export default class App extends React.Component {
+    state = {
+        value: ''
+    }
+    changeName = (e) => {
+        this.setState({
+            value: e.target.value
+        })
+    }
     render() {
         return(
             <div className='app'>
                 <Textarea maxlength={10} disable={false}/>
                 <Button btnSize={'gfg'} value={10}/>
-                <Input />
+                <Input onHandler={this.changeName}/>
+                <p>{this.state.value}</p>
             </div>
         )
     }
--- src/components/Input.js
+++ src/components/Input.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 class Input extends React.Component {
     checkInputValue = (e) => {
+        if (typeof this.props.onHandler === 'function') {
+            this.props.onHandler(e);
+
+        }
         e.target.value = e.target.value.replace(/\d/,'')
     };
     render() {
