09c55e8dd65ff5d8595f8983bc39a9a743817636
Andriy Sagan <aenain395@gmail.com>
2018-03-31 16:34:38 +0300

    Andriy (#3)
    * add textarea

---
+++ src/components/Textarea.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Textarea = (props) =>
+    <textarea name="textarea" id="text_area_component" cols="20" rows="5" maxLength={props.maxlength} disabled={props.disable}/>
+
+Textarea.propTypes = {
+    maxLength: PropTypes.number,
+    disabled: PropTypes.string
+};
+
+export default Textarea
