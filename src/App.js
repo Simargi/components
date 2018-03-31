5befda18bec2616063e7bb2592f318147b8e5710
Simargi <aenain395@gmail.com>
2018-03-31 15:13:10 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class App extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>1</h1>
+            </div>
+        )
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname +'/public',
+        filename: 'bundle.js'
+    },
+    watch: true,
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["react", 'es2015', 'stage-0'],
+                    plugins: ["transform-decorators-legacy"]
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader', {
+                    loader: 'postcss-loader',
+                    options: {
+                        plugins: () => [require('autoprefixer')]
+                    }}]
+            },
+            {
+                test: /\.scss/,
+                use: ['style-loader','css-loader', {
+                    loader: 'postcss-loader',
+                    options: {
+                        plugins: () => [require('autoprefixer')]
+                    }}, 'sass-loader']
+            }
+        ]
+    }
+};
